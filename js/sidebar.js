f2d7d3043f08f405c04b70f57729322d84f68ab7
Yumi Heo <hymsimple@gmail.com>
2023-07-31 16:29:22 +0900

    Feat : 사이드바 접고 펴기 기능 수정

--- js/sidebar.js
+++ js/sidebar.js
@@ -8,11 +8,20 @@ function clickSidebar() {
     toggleSidebar();
 }
 
+// 함수 내용 추가 7.31 허유미
+// 넓은 사이드바를 평소에는 안보이게 했다가, 클릭하면 보이도록 토글버튼 생성
 function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
+    const sidebar_wide = document.getElementById("sidebar-wide");
+    const top_menu = document.getElementById("top-menu");
+    const thumbnail = document.getElementById("thumbnail");
     if (isSidebarOpen) {
-        sidebar.style.width = "240px";
+        sidebar_wide.style.display = "flex";
+        top_menu.style.left = "240px"
+        thumbnail.style.marginLeft = "250px";
+
     } else {
-        sidebar.style.width = "65px";
+        sidebar_wide.style.display = "none";
+        top_menu.style.left = "65px"
+        thumbnail.style.marginLeft = "120px";
     }
 }
