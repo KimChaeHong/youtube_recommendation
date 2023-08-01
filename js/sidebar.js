01fa93f034fd588307a06dd1c2c5b617b46f9a9e
Yumi Heo <hymsimple@gmail.com>
2023-08-01 15:35:11 +0900

    Add: 홈화면 동영상 로딩 및 검색 기능

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 document.getElementById("menu-icon").addEventListener("click", clickSidebar);
 
-isSidebarOpen = true;
+isSidebarOpen = false;
 
 function clickSidebar() {
     isSidebarOpen = !isSidebarOpen;
@@ -11,17 +11,17 @@ function clickSidebar() {
 // 함수 내용 추가 7.31 허유미
 // 넓은 사이드바를 평소에는 안보이게 했다가, 클릭하면 보이도록 토글버튼 생성
 function toggleSidebar() {
-    const sidebar_wide = document.getElementById("sidebar-wide");
-    const top_menu = document.getElementById("top-menu");
-    const thumbnail = document.getElementById("thumbnail");
+    const sidebarWide = document.getElementById("sidebar-wide");
+    const topMenu = document.getElementById("top-menu");
+    const feed = document.getElementById("feed");
     if (isSidebarOpen) {
-        sidebar_wide.style.display = "flex";
-        top_menu.style.left = "240px"
-        thumbnail.style.marginLeft = "250px";
+        sidebarWide.style.display = "flex";
+        topMenu.style.left = "240px"
+        feed.style.marginLeft = "250px";
 
     } else {
-        sidebar_wide.style.display = "none";
-        top_menu.style.left = "65px"
-        thumbnail.style.marginLeft = "120px";
+        sidebarWide.style.display = "none";
+        topMenu.style.left = "65px"
+        feed.style.marginLeft = "120px";
     }
 }
