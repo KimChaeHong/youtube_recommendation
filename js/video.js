3f954ee207d867d7f61567b0271f28977cae97ba
hoi <coghd28@naver.com>
2023-08-07 13:26:49 +0900

    Feat: 영상불러오기

--- js/video.js
+++ js/video.js
@@ -1,108 +1,144 @@
 // 처음 화면 로드 시 전체 비디오 리스트 가져오기
-
 getVideoList().then(createVideoItem);
+
+// 현재 주소에서 비디오ID 가져오기
+let currentURL = window.location.href;
+let url = new URL(currentURL);
+let videoId = url.searchParams.get("id"); //채널명
+// videoId = "12";
 
 // 비디오 리스트 정보
 async function getVideoList() {
-    let response = await fetch("http://oreumi.appspot.com/video/getVideoList");
-    let videoListData = await response.json();
-    return videoListData;
+  let response = await fetch("http://oreumi.appspot.com/video/getVideoList");
+  let videoListData = await response.json();
+  return videoListData;
 }
 
 // 각 비디오 정보
 async function getVideoInfo(videoId) {
-    let url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
-    let response = await fetch(url);
-    let videoData = await response.json();
-    return videoData;
-}
-
-//채널 캐시정보 담을 객체 선언
-let channelCache = {};
+  let url = `http://oreumi.appspot.com/video/getVideoInfo?video_id=${videoId}`;
+  let response = await fetch(url);
+  let videoData = await response.json();
+  return videoData;
+}videoListDiv
 
 // 채널 정보
 async function getChannelInfo(channelName) {
-    // 캐시에 채널 정보가 있는지 확인
-    if (channelCache[channelName]) {
-        return channelCache[channelName];
-    }
+  let url = `http://oreumi.appspot.com/channel/getChannelInfo`;
 
-    let url = `http://oreumi.appspot.com/channel/getChannelInfo`;
+  let response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ video_channel: channelName }),
+  });
 
-    let response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ video_channel: channelName }),
-    });
-
-    let channelData = await response.json();
-
-    // 캐시에 채널 정보 저장
-    channelCache[channelName] = channelData;
-
-    return channelData;
+  let channelData = await response.json();
+  return channelData;
 }
 
+// 채널 내 영상정보
+async function getChannelVideo() {
+  let response = await fetch(
+    `http://oreumi.appspot.com/video/getChannelVideo?video_channel=${channelName}`
+  );
+  let videoListData = await response.json();
+  return videoListData;
+}
 
-// 피드 비디오 리스트 로드
+// 피드 내용 로드
 async function createVideoItem(videoList) {
-    let feed = document.getElementById("feed");
-    let feedItems = "";
+  let videoContainer = document.getElementById("video__container");
+  let videoTitle = document.getElementById("video__title");
+  let videoInfoBox = document.getElementById("info-text");
 
-    let videoInfoPromises = videoList.map((video) =>
-        getVideoInfo(video.video_id)
-    );
-    let videoInfoList = await Promise.all(videoInfoPromises);
+  // 현재 비디오 정보 가져오기
+  let currentVideoInfo = await getVideoInfo(videoId);
+  let tagList = currentVideoInfo.video_tag;
+  let channelName = currentVideoInfo.video_channel;
 
-    for (let i = 0; i < videoList.length; i++) {
-        let videoId = videoList[i].video_id;
-        let videoInfo = videoInfoList[i];
-        let channelInfo = await getChannelInfo(videoList[i].video_channel);
+  // 비디오 추가
+  videoContainer.innerHTML = `
+        <video id="current__video" controls autoplay muted>
+                    <source src="http://storage.googleapis.com/oreumi.appspot.com/video_${videoId}.mp4">
+        </video>
+    `;
 
-        let channelURL = `./channel.html?channelName=${videoList[i].video_channel}"`;
-        let videoURL = `./video.html?id=${videoId}"`;
+  videoTitle.innerHTML = `
+        <div id="video__title" class="video__title">
+            ${currentVideoInfo.video_title}
+        </div>
+    `;
+  videoInfoBox.innerHTML = `
+        <p>조회수 ${convertViews(currentVideoInfo.views)}회 • ${convertDate(
+    currentVideoInfo.upload_date
+  )}</p>
+    `;
 
-        // 조회수 표현
-        function formatViews(views) {
-            if (views >= 1000000) {
-                return `${(views / 1000000).toFixed(1).replace(/\.000$/, '')}M`;
-            } else if (views >= 1000) {
-                return `${(views / 1000).toFixed(1).replace(/\.0$/, '')}K`;
-            } else {
-                return `${views}`;
-            }
-        }
-        let simpleViews = formatViews(videoInfo.views);
+  // 추천 태그
+  let recoSortButtons = document.getElementById("reco__sort__buttons");
 
-        // 업로드 날짜 표현
-        function calculateTimeAgo(uploadDate) {
-            const now = new Date();
-            const upload = new Date(uploadDate);
-            const timeDiff = now - upload;
-            const daysAgo = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-            const monthsAgo = Math.floor(daysAgo / 30);
-            const yearsAgo = Math.floor(monthsAgo / 12);
+  recoSortButtons.innerHTML += `<button class="selected">${currentVideoInfo.video_channel}</button>`;
 
-            if (daysAgo === 0) {
-                return "today";
-            } else if (daysAgo === 1) {
-                return "yesterday";
-            } else if (monthsAgo < 1) {
-                return `${daysAgo}days ago`;
-            } else if (monthsAgo < 12) {
-                return `${monthsAgo}months ago`;
-            } else {
-                return `${yearsAgo}years ago`;
-            }
-        }
-        let uploadTimeAgo = calculateTimeAgo(videoInfo.upload_date);
+  for (let i = 0; i < tagList.length; i++) {
+    let tag = tagList[i];
 
+    recoSortButtons.innerHTML += `
+            <button>${tag}</button>
+            `;
+  }
 
+  let currentChannelInfo = await getChannelInfo(channelName);
+  let currentChannelURL = `./channel.html?channelName=${channelName}`;
+  let channelInfoBox = document.getElementById("channel__info__box");
+  channelInfoBox.innerHTML = `
+  <div id="channel-title">
+  <div id="frame11">
+      <div id="channel-profile-pic">
+          <div class="user-avatar">
+              <img src=${currentChannelInfo.channel_profile} alt="">
+          </div>
+      </div>
+      <div id="channel-profile-name">
+          <div id="channel-name">${currentChannelInfo.channel_name}</div>
+          <div id="subscribers">${convertViews(currentChannelInfo.subscribers)}</div>
+      </div>
+      <div id="channel-subscribes-btn">
+          <div id="sub-btn">SUBSCRIBES</div>
 
+      </div>
+  </div>
+</div>
+    `;
 
-        feedItems += `
+  let channelInfoDownSide = document.getElementById("channel__info__downside");
+  channelInfoDownSide.innerHTML = `
+    <p>${currentVideoInfo.video_detail}
+    </p>
+    <button>SHOW MORE</button>
+    `;
+
+  // 각 비디오들 정보 가져오기
+  let videoInfoPromises = videoList.map((video) =>
+    getVideoInfo(video.video_id)
+  );
+  let videoInfoList = await Promise.all(videoInfoPromises);
+  //채널명으로 필터링
+  let filteredVideoList = videoInfoList.filter(
+    (videoInfo) => videoInfo.video_channel === channelName
+  );
+
+  // 비디오리스트에 추가
+  let videoListDiv = document.getElementById("feed");
+  let feedItems = "";
+  for (let i = 0; i < filteredVideoList.length; i++) {
+    let video = filteredVideoList[i];
+    let channelName = video.video_channel;
+    let videoURL = `./video.html?id=${i}"`;
+    let channelURL = `./channel.html?channelName=${channelName}`;
+
+    feedItems += `
     <div id="video">
     <a href="https://www.youtube.com/watch?v=JXl4QgYUi9c&t=1s"></a>
     <div id="thumnail">
@@ -124,9 +160,73 @@ async function createVideoItem(videoList) {
         </ul>
     </div> 
 </div>
-    `;
-    }
+        `;
+  }
 
-    // 화면에 추가
-    feed.innerHTML = feedItems;
+  videoListDiv.innerHTML = feedItems;
+}
+
+// 단위 변환 함수
+function convertViews(views) {
+  if (views >= 10000000) {
+    const converted = (views / 10000000).toFixed(1);
+    return converted.endsWith(".0")
+      ? converted.slice(0, -2) + "천만"
+      : converted + "천만";
+  } else if (views >= 1000000) {
+    const converted = (views / 1000000).toFixed(1);
+    return converted.endsWith(".0")
+      ? converted.slice(0, -2) + "백만"
+      : converted + "백만";
+  } else if (views >= 10000) {
+    const converted = (views / 10000).toFixed(1);
+    return converted.endsWith(".0")
+      ? converted.slice(0, -2) + "만"
+      : converted + "만";
+  } else if (views >= 1000) {
+    const converted = (views / 1000).toFixed(1);
+    return converted.endsWith(".0")
+      ? converted.slice(0, -2) + "천"
+      : converted + "천";
+  } else {
+    return views.toString();
+  }
+}
+
+// 날짜 변환 함수
+function convertDate(dateString) {
+  // 파라미터로 받은 날짜를 Date 객체로 변환
+  const targetDate = new Date(dateString);
+
+  // 현재 날짜를 구하기 위해 현재 시간 기준으로 Date 객체 생성
+  const currentDate = new Date();
+
+  // 두 날짜의 시간 차이 계산 (밀리초 기준)
+  const timeDifference = currentDate - targetDate;
+
+  // 1년의 밀리초 수
+  const oneYearInMilliseconds = 31536000000;
+
+  if (timeDifference < 86400000) {
+    // 하루(24시간) 기준의 밀리초 수
+    return "오늘";
+  } else if (timeDifference < 172800000) {
+    // 이틀(48시간) 기준의 밀리초 수 (어제)
+    return "어제";
+  } else if (timeDifference < 604800000) {
+    // 일주일(7일) 기준의 밀리초 수
+    return "1주 전";
+  } else if (timeDifference < oneYearInMilliseconds) {
+    // 한 달 전 계산
+    const currentMonth = currentDate.getMonth();
+    const targetMonth = targetDate.getMonth();
+
+    if (currentMonth === targetMonth) {
+      return "1개월 전";
+    } else {
+      return `${currentMonth - targetMonth}개월 전`;
+    }
+  } else {
+    return `${Math.floor(timeDifference / oneYearInMilliseconds)}년 전`;
+  }
 }
